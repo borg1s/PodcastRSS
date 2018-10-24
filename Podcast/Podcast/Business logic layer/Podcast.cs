d59071573197506a95f98a1d0adcc422d085401d
NilssonKristian <nilsson_kristian@outlook.com>
2018-10-24 11:17:11 +0200

    Fyllt på variabler podcast-klassen

--- Podcast/Podcast/Business logic layer/Podcast.cs
+++ Podcast/Podcast/Business logic layer/Podcast.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 using System.ServiceModel.Syndication;
 
 namespace Podcast.Business_logic_layer
-{
-    class Podcast
+{ 
+    public class Podcast
     {
+        string namn { get; set; } //Testa att ta bort set; när metoden för att sätta namnet är skriven.
+        short avsnitt { get; set; }
+
     }
 }
